14c70efe259b80ff11da8c91e0088eccbaa7f695
Baloo54 <gabriel.comte2006@gmail.com>
2024-09-23 14:59:00 +0200

    matière.java

---
+++ collection/src/main/java/collection/Matiere.java
@@ -0,0 +1,22 @@
+package collection;
+/**
+ * classe matière.
+ */
+public class Matiere {
+    /**
+     * attribut nom.
+     */
+    private String nom;
+    /**
+     * constructeur.
+     */
+    public Matiere(String nom) {
+        this.nom = nom;
+    }
+    /**
+     * getter nom.
+     */
+    public String getNom() {
+        return nom;
+    }   
+}
